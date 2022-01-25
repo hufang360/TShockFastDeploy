5023d0c382b84b39a3ac1e25a3c2a0da3a8024fb
hufang360 <hufang360@sina.com>
2022-01-26 02:03:50 +0800

    首次提交

---
+++ TShockFastDeploy/TShockFastDeploy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Terraria.Localization;
+
+
+namespace Plugin
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Name => "快速开服";
+        public override string Description => "";
+        public override string Author => "hufang360";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public Plugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command(new List<string>() {}, InitCommand, "", "") { HelpText = ""});
+        }
+
+
+        private void InitCommand(CommandArgs args)
+        {
+
+        }
+
+
+        protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+			}
+			base.Dispose(disposing);
+		}
+	}
+
+}
