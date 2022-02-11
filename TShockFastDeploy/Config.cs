8b5f9dea859aeae2bf9b8f2a84d1d8ae6de69f71
hufang360 <hufang360@sina.com>
2022-02-12 06:09:38 +0800

    能修改 config.json

---
+++ TShockFastDeploy/Config.cs
@@ -0,0 +1,163 @@
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace Plugin
+{
+    public class Config{
+
+        // 默认用户组
+        public string Group = "default";
+
+        // 默认用户组拥有的权限
+        public List<string> Permissions = new List<string>();
+
+        // GM组拥有的权限
+        public List<string> GMPermissions = new List<string>();
+
+        // 开启SSC
+        public bool SSCEnable = true;
+
+        // ----------------------------------------
+
+        // 需要登录
+        public bool RequireLogin = true;
+
+        // 日志存放路径
+        public string LogPath = "tshock/logs";
+
+        // 日志调试
+        public bool DebugLogs = false;
+
+        // 保存地图提示
+        public bool AnnounceSave = false;
+
+        // 头顶聊天文字显示
+        public bool EnableChatAboveHeads = true;
+
+        // ftw的机械骷髅王不炸图（1.4.0.5 无此选项）
+        public bool DisablePrimeBombs = true;
+
+
+        // 重生秒数，原版默认是15秒
+        public int RespawnSeconds = 5;
+
+        // BOSS重生秒数，原版默认是30秒
+        public int RespawnBossSeconds = 10;
+
+        // ----------------------------------------
+
+        // // 墓碑生成
+        // public bool DisableTombstones = true;
+
+
+        // // 开启圣诞节
+        // public bool ForceXmas = false;
+
+        // // 开启万圣节
+        // public bool ForceHalloween = false;
+
+
+        public static Config Load(string path)
+        {
+            if (File.Exists(path))
+            {
+                return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+            }
+            else
+            {
+                var c = InitDefault();
+                File.WriteAllText(path, JsonConvert.SerializeObject(c, Formatting.Indented));
+                return c;
+            }
+        }
+
+        public static void Save(Config c, string path){
+            File.WriteAllText(path, JsonConvert.SerializeObject(c, Formatting.Indented));
+        }
+
+
+        public static Config InitDefault()
+        {
+            Config c = new Config();
+            c.Group = "default";
+            c.Permissions = GetDefaultPerms();
+            c.GMPermissions = GetGMPerms();
+
+            return c;
+        }
+
+        public static List<string> GetGMPerms()
+        {
+            // https://tshock.readme.io/docs/server-side-character-config
+           return new List<string>(){
+                "*",
+                "!tshock.ignore.ssc"
+           };
+        }
+
+
+        public static List<string> GetDefaultPerms()
+        {
+            // TShockAPI\DB\GroupManager.cs
+
+            // last
+            // https://github.com/Pryaxis/TShock/blob/general-devel/TShockAPI/DB/GroupManager.cs#L60
+
+            // 1.4.0.5
+            // https://github.com/Pryaxis/TShock/blob/f538ceb79371776afa386e9bc7648366f16b897c/TShockAPI/DB/GroupManager.cs
+            
+            // guest, default, vip, newadmin, admin, trustedadmin, owner
+            return new List<string>(){
+                // 4.5.12 default组 默认权限
+                // "tshock.warp",                                       // 传送点管理
+                "!tshock.warp",                                         // 移除传送点管理权限（在权限前添加!表示忽略这个权限）
+                // "tshock.account.changepassword",       // 更改密码
+                // "tshock.account.logout",             // 登出
+                // "tshock.npc.summonboss",			// 召唤boss
+                // "tshock.whisper",                        // 玩家私信
+                // "tshock.tp.wormhole",              // 虫洞药水
+                // "tshock.world.paint",				// 使用油漆
+
+                // 下面4条 1.4.0.5 的default组没有
+                "tshock.tp.pylon",                          // 传送晶塔
+                "tshock.tp.tppotion",                   // 传送药水
+                "tshock.tp.magicconch",             // 魔法海螺
+                "tshock.tp.demonconch",           // 恶魔海螺
+
+                // guest组默认权限
+                // "tshock.world.modify",           // 挖掘砍树建造
+                // "tshock.account.register",      //  注册
+                // "tshock.account.login",        //   登录
+                // "tshock.partychat",
+                // "tshock.thirdperson",
+                // "tshock.canchat",
+                // "tshock.synclocalarea",
+                // "tshock.sendemoji",
+
+                "tshock.tp.rod",                                 // 混沌传送法杖
+
+                "tshock.world.toggleparty",             // 开派对
+                "tshock.world.time.usesundial",     // 附魔日晷
+                "tshock.world.editspawn",			 // 设置出生点
+                "tshock.world.movenpc",			    // 为NPC分配房屋
+
+                "tshock.npc.hurttown",				// 伤害NPC
+                "tshock.npc.startinvasion",		  // 召唤入侵
+                "tshock.npc.startdd2",				// 天国事件
+                "tshock.npc.spawnpets",			 // 生成城镇宠物
+
+                "tshock.ignore.removetile",         // 忽略 乱挖砖块(使用炸弹等) 检测
+                "tshock.ignore.liquid",                // 忽略 液体 检测
+                "tshock.ignore.noclip",               // 忽略 穿墙 检测
+                "tshock.ignore.paint",  			  // 忽略 油漆 检测
+                "tshock.ignore.placetile",           // 忽略 替换方块 检测
+                "tshock.ignore.projectile",         // 忽略 射弹 检测
+                "tshock.ignore.damage",           // 忽略 高伤害 检测
+                "tshock.ignore.sendtilesquare",	// 允许 锤击改变飞镖机关方向
+            };
+        }
+
+    }
+}
--- TShockFastDeploy/TShockFastDeploy.cs
+++ TShockFastDeploy/TShockFastDeploy.cs
@@ -1,11 +1,13 @@
 ﻿using System.Linq;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.DB;
+
 
 
 namespace Plugin
@@ -18,149 +20,334 @@ namespace Plugin
         public override string Author => "hufang360";
         public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
 
+        private static Config _config;
+
+
+        private static string FDPath = Path.Combine(TShock.SavePath, "FastDeploy");
+        private static string FDConfigPath = Path.Combine(TShock.SavePath, "FastDeploy", "config.json");
+
+        private static string ConfigPath = Path.Combine(TShock.SavePath, "config.json");
+        private static string ServerSideCharacterConfigPath = Path.Combine(TShock.SavePath, "sscconfig.json");
+
+
         public Plugin(Main game) : base(game)
         {
         }
 
         public override void Initialize()
         {
-            Commands.ChatCommands.Add(new Command(new List<string>() {"fastdeploy"}, ProcessCommand, "fastdeploy", "fd") { HelpText = ""});
-            AddGMGroup();
-            AddDefaultPermission();
+            Commands.ChatCommands.Add(new Command(new List<string>() {"fastdeploy"}, FastDeploy, "fastdeploy", "fd") { HelpText = "快速开服"});
+
+            // 创建配置文件夹
+            if( !Directory.Exists(FDPath) )
+                Directory.CreateDirectory(FDPath);
         }
 
-        private void AddGMGroup()
+        // 重载配置
+        public void reload(bool force=false)
         {
-            string msg = "";
-            AddGroup("GM","*,!tshock.ignore.ssc", out msg);
-            TShock.Log.ConsoleInfo($"[FastDeploy]{msg}");
+            if( _config==null || force )
+                _config = Config.Load(FDConfigPath);
         }
 
-        private void AddDefaultPermission()
+        // 保存配置
+        public void Save(){
+            Config.Save(_config,  FDConfigPath);
+        }
+
+
+        private void FastDeploy(CommandArgs args)
+        {
+            reload();
+
+            TSPlayer op = args.Player;
+            void ShowHelpText()
+            {
+                op.SendInfoMessage("/fd init，快速处理好开服所需的权限和设置");
+                op.SendInfoMessage("/fd perm，给默认组设置常用权限，并新增GM超管组");
+                op.SendInfoMessage("/fd journey，授权默认组拥有旅行模式的全部权限");
+                op.SendInfoMessage("/fd add <指令>，授权使用某个指令");
+                op.SendInfoMessage("/fd del <指令>，取消某个指令的授权");
+                op.SendInfoMessage("/fd guest，设置插件的默认组为 guest");
+                op.SendInfoMessage("/fd default，设置插件的默认组为 default");
+                op.SendInfoMessage("/fd reload，重载配置");
+            }
+
+            if(args.Parameters.Count==0){
+                ShowHelpText();
+                return;
+            }
+
+            string cmd = args.Parameters[0].ToLowerInvariant();
+            switch (cmd)
+            {
+                // 快速开服
+                case "init":
+                    InitSever(op);
+                    return;
+
+                //  基础权限
+                case "perm":
+                    InitPerm(op);
+                    return;
+
+                //  基础权限
+                case "journey":
+                case "jour":
+                case "旅行":
+                case "旅途":
+                case "旅程":
+                    InitJourney(op);
+                    return;
+
+                //  将默认组设置成 default
+                //  将默认组设置成 guest
+                case "default":
+                case "guest":
+                    _config.Group = cmd;
+                    Save();
+                    return;
+
+                // 允许使用权限
+                case "add":
+                    AllowCommand(args);
+                    return;
+
+                // 不允许使用权限
+                case "del":
+                    DisallowCommand(args);
+                    return;
+
+                // 重载配置
+                case "reload":
+                    reload(true);
+                    op.SendSuccessMessage("[FastDeploy]配置已重载！");
+                    return;
+
+                // 帮助
+                case "help":
+                    ShowHelpText();
+                    return;
+            }
+        }
+
+
+        // 设置 快速开服
+        private void InitSever(TSPlayer op)
+        {
+            // 修改 sscconfig.json 更新此文件需重启服务器
+            TShock.ServerSideCharacterConfig.Settings.Enabled = _config.SSCEnable;
+            TShock.ServerSideCharacterConfig.Write(ServerSideCharacterConfigPath);
+
+            // 修改 config.json
+            TShock.Config.Settings.RequireLogin = _config.RequireLogin;
+            TShock.Config.Settings.LogPath = _config.LogPath;
+            TShock.Config.Settings.DebugLogs = _config.DebugLogs;
+            TShock.Config.Settings.AnnounceSave = _config.AnnounceSave;
+            TShock.Config.Settings.EnableChatAboveHeads = _config.EnableChatAboveHeads;
+            TShock.Config.Settings.DisablePrimeBombs = _config.DisablePrimeBombs;
+            TShock.Config.Settings.RespawnSeconds = _config.RespawnSeconds;
+            TShock.Config.Settings.RespawnBossSeconds = _config.RespawnBossSeconds;
+            TShock.Config.Write(ConfigPath);
+
+            // 重载配置
+            TShock.Utils.Reload();
+			TShockAPI.Hooks.GeneralHooks.OnReloadEvent(op);
+
+            string GetDesc(bool foo ){
+                return foo ? "开启":"关闭";
+            }
+
+            List<string> msgs = new List<string>(){
+                $"-----[FastDeploy]快速开服-----",
+                "已做了如下操作（强制开荒等设置，需重启才能生效）：",
+                $"1 {GetDesc(_config.SSCEnable)} 强制开荒（SSC）",
+                "",
+                $"2.1 服务器日志将保存在 {_config.LogPath}（LogPath={_config.LogPath}）",
+                $"2.2 {GetDesc(_config.RequireLogin)} 注册登录（RequireLogin={_config.RequireLogin}）",
+                $"2.3 {GetDesc(_config.RequireLogin)} 调试（DebugLogs={_config.DebugLogs}）",
+                $"2.4 {GetDesc(_config.AnnounceSave)} 保存地图提示（AnnounceSave={_config.AnnounceSave}）",
+                $"2.5 {GetDesc(_config.EnableChatAboveHeads)} 头顶聊天文字（EnableChatAboveHeads={_config.EnableChatAboveHeads}）",
+                $"2.6 {GetDesc(_config.DisablePrimeBombs)} 禁用机械骷髅王炸弹（DisablePrimeBombs={_config.DisablePrimeBombs}）",
+                $"2.7 {_config.RespawnSeconds}s 复活时间（RespawnSeconds={_config.RespawnSeconds}）",
+                $"2.8 {_config.RespawnBossSeconds}s Boss战复活时间（RespawnBossSeconds={_config.RespawnBossSeconds}）",
+                ""
+            };
+            op.SendSuccessMessage( string.Join("\n", msgs) );
+
+
+            // 设置权限
+            string msg = "";
+            bool success = AddPerm(_config.Group,  _config.Permissions, out msg);
+            if( success ){
+                op.SendSuccessMessage($"3 成功添加以下权限到默认组({_config.Group}):");
+                op.SendSuccessMessage( string.Join("\n", _config.Permissions) );
+                op.SendSuccessMessage( $"输入 [c/96FF96:{Specifier}group delperm {_config.Group} <权限...>] 可删除对应权限\n");
+            } else {
+                op.SendErrorMessage($"3 添加权限到默认组时失败！原因：{msg}\n");
+            }
+
+            msg = "";
+            // success = AddGroup("GM","*,!tshock.ignore.ssc", out msg);
+            success = AddGroup("GM",string.Join(",", _config.GMPermissions), out msg);
+            if( success ){
+                op.SendSuccessMessage($"4 已新建 GM 超管组，并添加了以下权限:");
+                op.SendSuccessMessage( string.Join("\n", _config.GMPermissions) );
+                op.SendSuccessMessage( $"输入 [c/96FF96:{Specifier}user group <玩家名> GM] 添加服主");
+
+                return;
+            }
+
+            if( msg!="组已存在" ){
+                op.SendErrorMessage($"操作失败！原因：{msg}");
+                return;
+            }
+
+            msg = "";
+            success = AddPerm("GM",  _config.GMPermissions, out msg);
+            if( success ){
+                op.SendSuccessMessage($"4 成功添加以下权限到 GM 超管组:");
+                op.SendSuccessMessage( string.Join("\n", _config.GMPermissions) );
+                op.SendSuccessMessage( $"输入 [c/96FF96:{Specifier}user group <玩家名> GM] 添加服主");
+            } else {
+                op.SendErrorMessage($"4 添加权限到 GM 超管组时失败！原因：{msg}");
+            }
+        }
+
+
+        // 设置权限
+        private void InitPerm(TSPlayer op)
+        {
+            AddDefaultPermission(op);
+            AddGMGroup(op);
+        }
+
+
+        // 设置旅行模式权限
+        private void InitJourney(TSPlayer op)
         {
             List<string> perms = new List<string>()
             {
-                // 4.5.12 default组 默认权限
-                // "tshock.account.changepassword",       // 更改密码
-                // "tshock.account.logout",             // 登出
-                // "tshock.npc.summonboss",			// 召唤boss
-                // "tshock.whisper",                        // 玩家私信
-                // "tshock.tp.wormhole",              // 虫洞药水
-                // "tshock.world.paint",				// 使用油漆
-                // "tshock.tp.pylon",                    // 传送晶塔
-                "tshock.tp.tppotion",                   // 传送药水
-                "tshock.tp.magicconch",             // 魔法海螺
-                "tshock.tp.demonconch",           // 恶魔海螺
-
-                // guest组默认权限
-                // "tshock.world.modify",           // 挖掘砍树建造
-                // "tshock.account.register",      //  注册
-                // "tshock.account.login",        //   登录
-                // "tshock.partychat",
-                // "tshock.thirdperson",
-                // "tshock.canchat",
-                // "tshock.synclocalarea",
-                // "tshock.sendemoji",
-
-                "tshock.tp.rod",                                 // 混沌传送法杖
-
-                "tshock.world.toggleparty",             // 开派对
-                "tshock.world.time.usesundial",     // 附魔日晷
-                "tshock.world.editspawn",			 // 设置出生点
-                "tshock.world.movenpc",			    // 为NPC分配房屋
-
-                "tshock.npc.hurttown",				// 伤害NPC
-                "tshock.npc.startinvasion",		  // 召唤入侵
-                "tshock.npc.startdd2",				// 天国事件
-                "tshock.npc.spawnpets",			 // 生成城镇宠物
-
-                "tshock.ignore.removetile",         // 忽略 乱挖砖块(使用炸弹等) 检测
-                "tshock.ignore.liquid",                // 忽略 液体 检测
-                "tshock.ignore.noclip",               // 忽略 穿墙 检测
-                "tshock.ignore.paint",  			  // 忽略 油漆 检测
-                "tshock.ignore.placetile",           // 忽略 替换方块 检测
-                "tshock.ignore.projectile",         // 忽略 射弹 检测
-                "tshock.ignore.damage",           // 忽略 高伤害 检测
-                "tshock.ignore.sendtilesquare",	// 允许 锤击改变飞镖机关方向
+                "tshock.journey.research",      // 物品复制
+                "tshock.journey.time.freeze",   //时间冻结
+                "tshock.journey.time.set",      // 时间调节
+                "tshock.journey.time.setspeed", //时间速度
+                "tshock.journey.wind.strength", //风强度
+                "tshock.journey.wind.freeze",       //风控制
+                "tshock.journey.rain.strength", // 雨强度
+                "tshock.journey.rain.freeze",   //雨改变
+                "tshock.journey.godmode",   // 无敌模式
+                "tshock.journey.placementrange",    // 扩大放置范围
+                "tshock.journey.setspawnrate",          // 敌人生成速度
+                "tshock.journey.biomespreadfreeze", //腐化传染
+                "tshock.journey.setdifficulty"              // 游戏难度
             };
+            // List<string> perms = TShockAPI.Handlers.NetModules.CreativePowerHandler.PermissionToDescriptionMap.Keys.ToList();
+            // if( !perms.Contains(Permissions.journey_contributeresearch) ){
+            //     perms.Add(Permissions.journey_contributeresearch);
+            // }
+
             string msg = "";
-            bool success = AddPerm("default", perms, out msg);
+            bool success = AddPerm(_config.Group, perms, out msg);
             if( success )
-                TShock.Log.ConsoleInfo($"[FastDeploy]{msg}");
-            else
-                TShock.Log.ConsoleError($"[FastDeploy]{msg}");
+            {
+                op.SendSuccessMessage($"成功添加以下权限到默认组({_config.Group}):");
+                op.SendSuccessMessage( string.Join("\n", perms) );
+                op.SendSuccessMessage( $"输入 /group delperm {_config.Group} <权限...> 可删除对应权限");
+            } else {
+                op.SendErrorMessage($"操作失败!原因：{msg}");
+            }
         }
 
 
-        private void ProcessCommand(CommandArgs args)
+        // 允许使用指令
+        private void AllowCommand(CommandArgs args)
         {
-            if(args.Parameters.Count==0){
-                args.Player.SendErrorMessage("语法错误，请输入 /fd help 查看帮助");
+            TSPlayer op = args.Player;
+            if(args.Parameters.Count<2){
+                op.SendErrorMessage("语法错误，需要输入指令名称, 例如：/fd add tpnpc");
                 return;
             }
+            string cmdStr = args.Parameters[1];
 
-            TSPlayer op = args.Player;
-            List<string> perms = new List<string>();
-            string cmd = args.Parameters[0].ToLowerInvariant();
-
-            if( _perms.Keys.Contains( cmd ) )
+            foreach (Command cmd in Commands.ChatCommands)
             {
-                perms.Add(_perms[cmd]);
-            } else {
-                switch (cmd)
+                if( cmd.Names.Contains(cmdStr) )
                 {
-                    // case "init":
-                    //     // 添加默认组
-                    //     AddGMGroup();
-                    //     // 给默认组添加权限
-                    //     AddDefaultPermission();
-                    //     break;
-
-                    case "journey":
-                    case "jour":
-                    case "旅行":
-                    case "旅途":
-                        perms = new List<string>()
-                        {
-                            "tshock.journey.research",
-                            "tshock.journey.biomespreadfreeze",
-                            "tshock.journey.rain.freeze",
-                            "tshock.journey.time.freeze",
-                            "tshock.journey.wind.freeze",
-                            "tshock.journey.placementrange"
-                        };
-                        break;
-                    
-                    default:
-                        op.SendErrorMessage($"暂时未处理与 {cmd} 指令有关的权限！");
-                        return;
+                    string msg = "";
+                    bool success = AddPerm(_config.Group, cmd.Permissions, out msg);
+                    if( success )
+                    {
+                        op.SendSuccessMessage($"成功添加以下权限到默认组({_config.Group}):");
+                        op.SendSuccessMessage( string.Join("\n", cmd.Permissions) );
+                    } else {
+                        op.SendErrorMessage($"操作失败!原因：{msg}");
+                    }
                 }
             }
+        }
 
-            if( perms.Count==0 )
+
+        // 不允许使用指令
+        private void DisallowCommand(CommandArgs args)
+        {
+            TSPlayer op = args.Player;
+            if(args.Parameters.Count<2){
+                op.SendErrorMessage("语法错误，需要输入指令名称, 例如：/fd del tpnpc");
                 return;
+            }
+            string cmdStr = args.Parameters[1];
 
-            string msg = "";
-            bool success = AddPerm("default", perms, out msg);
-
-            if( success ){
-                args.Player.SendSuccessMessage("成功添加以下权限到默认用户组:");
-                args.Player.SendSuccessMessage( string.Join("\n", perms) );
-            } else {
-                args.Player.SendErrorMessage($"操作失败!原因：{msg}");
+            foreach (Command cmd in Commands.ChatCommands)
+            {
+                if( cmd.Names.Contains(cmdStr) )
+                {
+                    string msg = "";
+                    bool success = DeletePerm(_config.Group, cmd.Permissions, out msg);
+                    if( success )
+                    {
+                        op.SendSuccessMessage($"已从默认组({_config.Group})移除了如下权限:");
+                        op.SendSuccessMessage( string.Join("\n", cmd.Permissions) );
+                    } else {
+                        op.SendErrorMessage($"操作失败!原因：{msg}");
+                    }
+                }
             }
         }
 
-        static Dictionary<string, string> _perms = new Dictionary<string, string>
+
+        // 为默认组添加权限
+        private void AddDefaultPermission(TSPlayer op)
         {
-            {"tpnpc", Permissions.tpnpc },
-            {"home", Permissions.home },
-            {"me", Permissions.cantalkinthird },
-            {"register", Permissions.canregister }
-        };
+            string msg = "";
+            bool success = AddPerm(_config.Group,  _config.Permissions, out msg);
+            if( success ){
+                op.SendSuccessMessage($"成功添加以下权限到默认组({_config.Group}):");
+                op.SendSuccessMessage( string.Join("\n", _config.Permissions) );
+                op.SendSuccessMessage( $"输入 /group delperm <组名> <权限...> 可删除对应权限");
+            } else {
+                op.SendErrorMessage($"操作失败！原因：{msg}");
+            }
+        }
 
 
+         // 新建GM组
+        private void AddGMGroup(TSPlayer op)
+        {
+            string msg = "";
+            bool success = AddGroup("GM","*,!tshock.ignore.ssc", out msg);
+            if( success ){
+                op.SendSuccessMessage("已新增 GM 超管组，输入 [c/96FF96:/user group <玩家名> GM] 添加服主");
+            } else {
+                if( msg=="组已存在" )
+                    op.SendInfoMessage("组已存在，输入 [c/96FF96:/user group <玩家名> GM] 添加服主");
+                else
+                    op.SendErrorMessage($"操作失败！原因：{msg}");
+            }
+        }
 
+
+        // 添加组方法
         private bool AddGroup(string groupName, string permissions, out string result)
         {
             try
@@ -170,7 +357,7 @@ namespace Plugin
             }
             catch (GroupExistsException)
             {
-                result = "组已存在!";
+                result = "组已存在";
                 return false;
             }
             catch (GroupManagerException ex)
@@ -181,6 +368,8 @@ namespace Plugin
             return true;
         }
 
+
+        // 添加权限方法
         private bool AddPerm(string groupName, List<string> permissions, out string result)
         {
             result = "";
@@ -199,6 +388,32 @@ namespace Plugin
             }
             return true;
         }
+
+
+        // 删除权限
+        private bool DeletePerm(string groupName, List<string> permissions, out string result)
+        {
+            result = "";
+            try
+            {
+                string response = TShock.Groups.DeletePermissions(groupName, permissions);
+                if (response.Length > 0)
+                {
+                    result = response;
+                }
+            }
+            catch (GroupManagerException ex)
+            {
+                result = ex.ToString();
+                return false;
+            }
+            return true;
+        }
+
+        internal static string Specifier
+		{
+			get { return Commands.Specifier; }
+		}
 
     }
 }
